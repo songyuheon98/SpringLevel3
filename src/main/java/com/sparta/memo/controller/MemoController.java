a796f972bf7b8d4bb789425a97cd2966c189b960
강현욱 <bug6868@naver.com>
2023-08-28 13:24:29 +0900

    스프링 입문 과제 1

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -9,10 +9,7 @@ import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.List;
 
 @RestController
@@ -26,20 +23,22 @@ public class MemoController {
     }
 
     @PostMapping("/memos")
-    public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
+    public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) { // 3번 요청 완성
         // RequestDto -> Entity
         Memo memo = new Memo(requestDto);
 
         // DB 저장
         KeyHolder keyHolder = new GeneratedKeyHolder(); // 기본 키를 반환받기 위한 객체
 
-        String sql = "INSERT INTO memo (username, contents) VALUES (?, ?)"; // 동적으로 받아줌
+        String sql = "INSERT INTO gallery (user_pw, username, title, contents) VALUES (?, ?, ?, ?)";
         jdbcTemplate.update( con -> {
                     PreparedStatement preparedStatement = con.prepareStatement(sql,
                             Statement.RETURN_GENERATED_KEYS);
 
-                    preparedStatement.setString(1, memo.getUsername()); // 첫번째 ? 에는 이름
-                    preparedStatement.setString(2, memo.getContents()); // 두번째 ? 에는 글 저장
+                    preparedStatement.setString(1, memo.getUser_pw());
+                    preparedStatement.setString(2, memo.getUsername());
+                    preparedStatement.setString(3, memo.getTitle());
+                    preparedStatement.setString(4, memo.getContents());
                     return preparedStatement;
                 },
                 keyHolder);
@@ -55,61 +54,96 @@ public class MemoController {
     }
 
     @GetMapping("/memos")
-    public List<MemoResponseDto> getMemos() {
+    public List<MemoResponseDto> getMemos() { // 일단 get도 잘된다.
         // DB 조회
-        String sql = "SELECT * FROM memo";
+        String sql = "SELECT title, username, contents, updateAt FROM gallery ORDER BY updateAt DESC";
 
         return jdbcTemplate.query(sql, new RowMapper<MemoResponseDto>() {
             @Override
             public MemoResponseDto mapRow(ResultSet rs, int rowNum) throws SQLException {
                 // SQL 의 결과로 받아온 Memo 데이터들을 MemoResponseDto 타입으로 변환해줄 메서드
-                Long id = rs.getLong("id");
+                String title = rs.getString("title");
                 String username = rs.getString("username");
                 String contents = rs.getString("contents");
-                return new MemoResponseDto(id, username, contents); // 메모 정보 리스트 타입으로 반환
+                Timestamp updateAt = rs.getTimestamp("updateAt");
+                return new MemoResponseDto(title, username, contents, updateAt);
             }
         });
     }
 
+    @GetMapping("/memos/{id}")
+    public MemoResponseDto getOneMemo(@PathVariable Long id) {
+        // DB 조회
+        Memo memo = findById(id);
+
+        if (memo != null) {
+            String sql = "SELECT title, username, updateAt, contents FROM gallery WHERE id = ?";
+            return jdbcTemplate.queryForObject(sql, new RowMapper<MemoResponseDto>() {
+                @Override
+                public MemoResponseDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+                    // SQL 의 결과로 받아온 Memo 데이터들을 MemoResponseDto 타입으로 변환해줄 메서드
+                    String title = rs.getString("title");
+                    String username = rs.getString("username");
+                    Timestamp updateAt = rs.getTimestamp("updateAt");
+                    String contents = rs.getString("contents");
+                    return new MemoResponseDto(title, username, contents, updateAt);
+                }
+            }, id);
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
+
     @PutMapping("/memos/{id}")
-    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+    public MemoResponseDto updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) { // 비번검사, 변경 성공, 시간도 성공
         // 해당 메모가 DB에 존재하는지 확인
         Memo memo = findById(id);
-        if(memo != null) {
-            // memo 내용 수정
-            String sql = "UPDATE memo SET username = ?, contents = ? WHERE id = ?"; // 동적으로 받기
-            jdbcTemplate.update(sql, requestDto.getUsername(), requestDto.getContents(), id);
+        String getPasswordQuery = "SELECT user_pw FROM gallery WHERE id = ?";
+        String actualPassword = jdbcTemplate.queryForObject(getPasswordQuery, String.class, id);
 
-            return id;
+        if(memo != null && actualPassword.equals(requestDto.getUser_pw())) {
+            // memo 내용 수정
+            String sql = "UPDATE gallery SET title = ?, username = ?, contents = ? WHERE id = ?";
+            jdbcTemplate.update(sql, requestDto.getTitle(), requestDto.getUsername(), requestDto.getContents(), id);
+
+            Memo updatedMemo = findById(id);
+
+            // Entity -> ResponseDto
+            return new MemoResponseDto(updatedMemo);
         } else {
             throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
         }
     }
 
     @DeleteMapping("/memos/{id}")
-    public Long deleteMemo(@PathVariable Long id) {
+    public String deleteMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) { // 성공
         // 해당 메모가 DB에 존재하는지 확인
         Memo memo = findById(id);
-        if(memo != null) {
+        String getPasswordQuery = "SELECT user_pw FROM gallery WHERE id = ?";
+        String actualPassword = jdbcTemplate.queryForObject(getPasswordQuery, String.class, id);
+        if(memo != null && actualPassword.equals(requestDto.getUser_pw())) {
             // memo 삭제
-            String sql = "DELETE FROM memo WHERE id = ?";
+            String sql = "DELETE FROM gallery WHERE id = ?";
             jdbcTemplate.update(sql, id);
 
-            return id;
+            return "삭제 성공";
         } else {
             throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
         }
     }
 
-    private Memo findById(Long id) { // 많이 쓰는 기능 함수 하나 만들어둠
+    private Memo findById(Long id) {
         // DB 조회
-        String sql = "SELECT * FROM memo WHERE id = ?";
+        String sql = "SELECT * FROM gallery WHERE id = ?";
 
         return jdbcTemplate.query(sql, resultSet -> {
             if(resultSet.next()) {
                 Memo memo = new Memo();
+                memo.setUser_pw(resultSet.getString("user_pw"));
+                memo.setTitle(resultSet.getString("title"));
                 memo.setUsername(resultSet.getString("username"));
                 memo.setContents(resultSet.getString("contents"));
+                memo.setUpdatedAt(resultSet.getTimestamp("updateAt"));
                 return memo;
             } else {
                 return null;
--- src/main/java/com/sparta/memo/dto/MemoRequestDto.java
+++ src/main/java/com/sparta/memo/dto/MemoRequestDto.java
@@ -1,9 +1,17 @@
 package com.sparta.memo.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
+
+import java.sql.Timestamp;
 
 @Getter
 public class MemoRequestDto { // 정보 주는 Dto
+    private Long id;
+    private String user_pw;
+
+    private String title;
     private String username;
     private String contents;
+    private Timestamp updatedAt;
 }
