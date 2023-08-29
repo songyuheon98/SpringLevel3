7e87d51c1be83da4e94bb809bbec29d40d53481b
강현욱 <bug6868@naver.com>
2023-08-29 14:24:05 +0900

    강한 결합을 느슨한 결합으로, ioc 컨테이너와 bean

--- src/main/java/com/sparta/memo/MemoApplication.java
+++ src/main/java/com/sparta/memo/MemoApplication.java
@@ -3,7 +3,7 @@ package com.sparta.memo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // 하위 패키지 모두 찾아서 @Componet들 Bean으로 등록
 public class MemoApplication {
 
 	public static void main(String[] args) {
