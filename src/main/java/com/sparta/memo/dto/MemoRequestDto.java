3d7f1c5af2580d5c458363dbfc4fa156289203da
강현욱 <bug6868@naver.com>
2023-08-29 13:42:13 +0900

    역할 분리하기

--- src/main/java/com/sparta/memo/dto/MemoRequestDto.java
+++ src/main/java/com/sparta/memo/dto/MemoRequestDto.java
@@ -8,10 +8,7 @@ import java.sql.Timestamp;
 @Getter
 public class MemoRequestDto { // 정보 주는 Dto
     private Long id;
-    private String user_pw;
-
-    private String title;
     private String username;
     private String contents;
-    private Timestamp updatedAt;
+
 }
